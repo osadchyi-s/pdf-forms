e3f269c6f46e6bb114ae7c879bd921862840f6e5
Andriy <andriy.matviychuk@pdffiller.team>
2017-02-14 17:39:13 +0200

    MAO | add style to 'success'

--- App/public/shortcodes/js/fillable-form.js
+++ App/public/shortcodes/js/fillable-form.js
@@ -3,7 +3,10 @@
     $('.pdfform-form').on('submit', function(event) {
         event.preventDefault();
         $_messages = $(this).children('.pdfform-messages');
-        $_messages.append( "Loading..." );
+        $_messages.empty().append( "Loading..." ).show().css({
+            "position": "absolute",
+            "margin-top":"-25px"
+        });
         $.post(window.PdfFormGlobalVariables.PdfformFillableForm.adminAjaxUrl, $(this).serialize(), function(response, status) {
             if (!window.PdfFormGlobalVariables.PdfformFillableForm.messageSuccess) {
                 window.PdfFormGlobalVariables.PdfformFillableForm.messageSuccess = 'Success';
