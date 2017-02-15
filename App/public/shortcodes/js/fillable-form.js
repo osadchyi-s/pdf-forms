d827a4d88e538702f7f106e4eb442b26d0f455dc
Andriy <andriy.matviychuk@pdffiller.team>
2017-02-15 11:18:25 +0200

    MAO | add block and reset to dorm inputs

--- App/public/shortcodes/js/fillable-form.js
+++ App/public/shortcodes/js/fillable-form.js
@@ -7,14 +7,18 @@
             "position": "absolute",
             "margin-top":"-25px"
         });
+        $('.pdfform-form input').attr('disabled', 'disabled');
         $.post(window.PdfFormGlobalVariables.PdfformFillableForm.adminAjaxUrl, $(this).serialize(), function(response, status) {
             if (!window.PdfFormGlobalVariables.PdfformFillableForm.messageSuccess) {
                 window.PdfFormGlobalVariables.PdfformFillableForm.messageSuccess = 'Success';
+                $('.pdfform-form input').removeAttr('disabled');
+                $('.pdfform-form')[0].reset();
             }
             $_messages.html(window.PdfFormGlobalVariables.PdfformFillableForm.messageSuccess).show('slow').delay(8000).fadeOut();
         }).fail( function(response, status) {
             if (!window.PdfFormGlobalVariables.PdfformFillableForm.messageFail) {
                 window.PdfFormGlobalVariables.PdfformFillableForm.messageFail = 'Fail';
+                $('.pdfform-form input').removeAttr('disabled');
             }
             $_messages.html(window.PdfFormGlobalVariables.PdfformFillableForm.messageFail).show('slow').delay(8000).fadeOut();
         });
