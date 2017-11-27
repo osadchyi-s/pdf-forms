10b5c9f3c0a94c22ddc26f15bbb0880164318a29
Osadchyi Serhii <sergijlys@gmail.com>
2017-11-27 08:22:26 +0200

    Version 0.1.10

--- pdf_forms.php
+++ pdf_forms.php
@@ -3,7 +3,7 @@
 Plugin Name: PDF Form Filler
 Plugin URI: https://github.com/pdffiller/wp-integration-pdf-forms
 Description: Fill and send form
-Version: 0.1.9
+Version: 0.1.10
 Author: PDFFiller
 Author URI: https://github.com/pdffiller
 Text Domain: pdf-form
